179414340d91ff6d4243859ee79816375910e5e7
Leonardo Grigorio Ferreira <leogrigs@MacBook-Air-de-LeoGrigs.local>
2024-04-16 11:42:21 -0300

    feat: pomodoro v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Button from "./components/button/button";
+import Timer from "./components/timer/timer";
+
+// TODO: Style
+
+const POMODORO_TIME = 20;
+const REST_TIME = 5;
 
 function App() {
+  const [isOn, setIsOn] = useState(false);
+  const [time, setTime] = useState(POMODORO_TIME);
+  const [onFocus, setOnFocus] = useState(true);
+  const label = isOn ? "Stop" : "Start";
+
+  useEffect(() => {
+    if (isOn) {
+      const interval = setInterval(() => {
+        setTime((prevTime) => {
+          console.log("interval");
+          if (prevTime === 1) {
+            setIsOn(false);
+            return 0;
+          } else {
+            return prevTime - 1;
+          }
+        });
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [isOn]);
+
+  useEffect(() => {
+    handleReset();
+  }, [onFocus]);
+
+  const handleToggle = () => {
+    setIsOn((isOn) => !isOn);
+  };
+
+  const handleReset = () => {
+    setIsOn(false);
+    setTime(getInitialTime());
+  };
+
+  const getInitialTime = () => {
+    return (onFocus ? POMODORO_TIME : REST_TIME) * 60;
+  };
+
+  const handleNext = () => {
+    setOnFocus((onFocus) => !onFocus);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <Timer time={time} />
+        <div className="container-button">
+          <Button label={label} onClick={handleToggle} />
+          <Button label={"Reset"} onClick={handleReset} />
+          <Button label={">"} onClick={handleNext} />
+        </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/button/button.js
@@ -0,0 +1,9 @@
+import "./button.css"
+
+export default function Button({label, onClick}) {
+    return (
+       <button className="button" onClick={onClick}>
+            {label}
+       </button>
+    )
+}
