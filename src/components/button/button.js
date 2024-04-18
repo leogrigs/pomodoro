7097257bd09de7c791a161b6e81b9e47f0ac5df7
leogrigs <68450578+leogrigs@users.noreply.github.com>
2024-04-18 10:50:39 +0000

    Deploying to main from @ leogrigs/easy-focus@336b9d1e131af25d8e0ed27bac3a9c93268f3700 🚀

--- src/components/button/button.js
+++
@@ -1,9 +0,0 @@
-import "./button.css";
-
-export default function Button({ label, icon: Icon, onClick }) {
-  return (
-    <button className="button" onClick={onClick}>
-      <Icon size={48} />
-    </button>
-  );
-}
