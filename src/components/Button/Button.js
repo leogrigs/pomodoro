fd4734051335f024607196197e459e07bdf3fde8
Leonardo Grigorio Ferreira <leo.grigorio16@gmai.com>
2024-06-25 11:11:54 -0300

    refactor: file name

--- src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import "./Button.css";
-
-export default function Button({ label, icon: Icon, className, onClick }) {
-  return (
-    <button className="button" onClick={onClick}>
-      <Icon className={className} />
-    </button>
-  );
-}
